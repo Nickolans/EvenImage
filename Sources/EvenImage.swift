d7eaae5fc1da955cd3eb4f387f16ea19cb738ed5
Nick Griffith <nickolansgriffith@gmail.com>
2024-05-14 23:29:17 -0500

    refactored logic out of main func

--- Sources/EvenImage.swift
+++ Sources/EvenImage.swift
@@ -6,7 +6,6 @@
 
 import ArgumentParser
 import CoreGraphics
-import QuartzCore
 import AppKit
 
 fileprivate extension NSColor {
@@ -15,8 +14,17 @@ fileprivate extension NSColor {
     }
     
     static var strokeGray: NSColor {
-        return NSColor(red: 20/255, green: 20/255, blue: 20/255, alpha: 1.0)
+        return NSColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1.0)
     }
+}
+
+fileprivate enum EvenImageError: Error {
+    case errorSavingImageFile
+    case errorRenderingImage
+    case imageUrlInvalid
+    case imageDataInvalid
+    case imagePathInvalid
+    case outputPathInvalid
 }
 
 @available(macOS 13.0, *)
@@ -29,8 +37,11 @@ struct EvenImage: ParsableCommand {
     @Option() var width = 2800
     
     mutating func run() throws {
-        guard let imagePath = relativeUrl(to: self.imagePath) else { return }
-        processImage(withPath: "file://\(imagePath.path)")
+        guard let imagePath = relativeUrl(to: self.imagePath) else {
+            throw EvenImageError.imagePathInvalid
+        }
+        
+        try processImage(withPath: "file://\(imagePath.path)")
     }
     
     private func relativeUrl(to path: String) -> URL? {
@@ -38,37 +49,52 @@ struct EvenImage: ParsableCommand {
         return URL(fileURLWithPath: path, relativeTo: currentPathUrl)
     }
     
-    private func processImage(withPath path: String) {
+    private func processImage(withPath path: String) throws {
         
-        guard let url = URL(string: path) else { return }
-        guard let data = try? Data(contentsOf: url) else { return }
-        let image = NSImage(data: data)!
+        guard let url = URL(string: path) else {
+            throw EvenImageError.imageUrlInvalid
+        }
+        
+        guard let data = try? Data(contentsOf: url), let image = NSImage(data: data) else {
+            throw EvenImageError.imageDataInvalid
+        }
         
         let context = NSGraphicsContext(cgContext: CGContext(data: nil, width: width, height: height,
-                                                         bitsPerComponent: 8,
-                                                         bytesPerRow: 0,
-                                                         space: CGColorSpaceCreateDeviceRGB(),
+                                                             bitsPerComponent: 8,
+                                                             bytesPerRow: 0,
+                                                             space: CGColorSpaceCreateDeviceRGB(),
                                                              bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue)!,
                                         flipped: false)
         
         
-        /* --- Background --- */
+        /* --- Background Color --- */
         
         context.cgContext.setFillColor(NSColor.backgroundGray.cgColor)
         context.cgContext.fill([CGRect(x: 0, y: 0, width: width, height: height)])
         
         /* --- Overlay Image --- */
         
+        try overlayImage(image, withContext: context)
         
+        /* --- Stroke --- */
+        
+        context.cgContext.setStrokeColor(NSColor.strokeGray.cgColor)
+        context.cgContext.stroke(CGRect(x: 0, y: 0, width: width, height: height), width: 15)
+        
+        try saveImage(withContext: context)
+    }
+    
+    private func overlayImage(_ image: NSImage, withContext context: NSGraphicsContext) throws {
+       
         var xOff: CGFloat = 0
         var yOff: CGFloat = 0
-        
         let widthRatio = CGFloat(width) / image.size.width
         let heightRatio = CGFloat(height) / image.size.height
         let scaleFactor = min(widthRatio, heightRatio)
+        let hostImageWidth = image.size.width * scaleFactor
+        let hostImageHeight = image.size.height * scaleFactor
         
-        var hostImageWidth = image.size.width * scaleFactor
-        var hostImageHeight = image.size.height * scaleFactor
+        /* --- Centering Overlay Image --- */
         
         if (CGFloat(width) - hostImageWidth) > 0 {
             xOff = (CGFloat(width) - hostImageWidth) / 2
@@ -78,35 +104,33 @@ struct EvenImage: ParsableCommand {
             yOff = (CGFloat(height) - hostImageHeight) / 2
         }
         
-        print(hostImageWidth, hostImageHeight)
-        
-        if let cgHostImage = image.cgImage(forProposedRect: nil, context: context, hints: nil) {
-            context.cgContext.draw(cgHostImage, in: .init(x: xOff, y: yOff, width: hostImageWidth, height: hostImageHeight), byTiling: false)
-        } else {
-            print("Image can't be made lol")
+        guard let cgHostImage = image.cgImage(forProposedRect: nil, context: context, hints: nil) else {
+            throw EvenImageError.errorRenderingImage
         }
         
-        context.cgContext.setStrokeColor(NSColor.strokeGray.cgColor)
-        context.cgContext.stroke(CGRect(x: 0, y: 0, width: width, height: height), width: 15)
-        context.cgContext.saveGState()
+        context.cgContext.draw(cgHostImage, in: .init(x: xOff, y: yOff, width: hostImageWidth, height: hostImageHeight), byTiling: false)
+    }
+    
+    private func saveImage(withContext context: NSGraphicsContext) throws {
+        guard let image = context.cgContext.makeImage() else {
+            throw EvenImageError.errorRenderingImage
+        }
         
-        print("1...")
-        guard let i = context.cgContext.makeImage() else { return }
-        let bitImageRep: NSBitmapImageRep = NSBitmapImageRep(cgImage: i)
+        let bitImageRep: NSBitmapImageRep = NSBitmapImageRep(cgImage: image)
         let result = bitImageRep.representation(using: .jpeg, properties: [:])
-        guard let data = result else { return }
         
-        print(FileManager.default.currentDirectoryPath)
+        guard let data = result else {
+            throw EvenImageError.errorRenderingImage
+        }
         
-        let currentPath = FileManager.default.homeDirectoryForCurrentUser
-        let path = relativeUrl(to: outputDirectory)!.appending(path: "\(UUID().uuidString).png")
-        
-        print("SAVING... ", path)
+        guard let path = relativeUrl(to: outputDirectory) else {
+            throw EvenImageError.outputPathInvalid
+        }
         
         do {
-            try data.write(to: path)
+            try data.write(to: path.appending(path: "\(UUID().uuidString).png"))
         } catch {
-            print("ERROR: ", error.localizedDescription)
+            throw EvenImageError.errorSavingImageFile
         }
     }
 }
